7bf0365b082804f0c66272e2149d68978aee42e9
Fuad Ragib <18370135+fuadragib01@users.noreply.github.com>
2022-03-13 22:34:22 +0600

    Create start-end-char-remove.php
    A PHP program to remove any character from start and end of a string.

---
+++ start-end-char-remove.php
@@ -0,0 +1,6 @@
+<?php 
+
+$text = "-Hello Themelooks-"; // Original string
+
+$textAfterRemove = substr( $text, 1, -1 ); // Removing first and last character from the string
+echo $textAfterRemove; // Displaying after removing character from start and end 
