b7a15aa8d8b4f63d3f8d81da4aff326a577752ac
Fuad Ragib <ragibfuad@gmail.com>
2022-07-04 10:02:27 +0600

    Create nested-switch.php

---
+++ nested-switch.php
@@ -0,0 +1,24 @@
+<?php
+$n = -12;
+$r = $n % 2;
+
+switch( $r ) {
+    case 0; // : or ; both is ok here
+        switch( $n ) {
+            case $n > 0;
+                echo "$n is a positive even number";
+                break;
+            case $n < 0;
+                echo "$n is a negative even number";
+        }
+        break;
+    default; // : or ; both is ok here 
+        switch( $n ) {
+            case $n > 0;
+                echo "$n is a positive odd number";
+                break;
+            case $n < 0;
+                echo "$n is a negative odd number";
+        }
+        break;
+}
