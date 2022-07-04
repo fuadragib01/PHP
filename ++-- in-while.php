cd3f50a9e2f0e2cf5c3f3993c6ed27722f8241a9
Fuad Ragib <ragibfuad@gmail.com>
2022-07-04 19:49:16 +0600

    Create ++-- in-while.php

---
+++ ++-- in-while.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * ++ and -- in while loop is very scensetive
+ * need to be very careful when handle it
+ */
+
+
+$j = 1;
+
+while ( ++$j < 10 ) {
+    echo $j . " ";
+}
+
+echo "\n";
+$j = 1;
+
+while ( $j++ < 10 ) {
+    echo $j . " ";
+}
