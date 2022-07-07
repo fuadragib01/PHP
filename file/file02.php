f8ad8a022090fc08e3a812fc8478d595b1c5639b
Faad Ragib <ragibfuad@gmail.com>
2022-07-07 17:05:54 +0600

    is_writable added

--- file/file02.php
+++ file/file02.php
@@ -1,22 +1,23 @@
 <?php
 
 $path = "E:\\PHP\\file\\text02.txt";
+if ( is_writable( $path ) ) {
+    $fp = fopen( $path, 'w' );
+    fwrite( $fp, "Fuad Ragib\n" );
 
-$fp = fopen( $path, 'w' );
-fwrite( $fp, "Fuad Ragib\n" );
+    // problems in 'w' mode
+    // this will delete all previous data from file, and will write only new data to the file
+    fwrite( $fp, "Fahad Nasif\n" );
+    fwrite( $fp, "Zawad Nazib\n" );
 
-// problems in 'w' mode
-// this will delete all previous data from file, and will write only new data to the file
-fwrite( $fp, "Fahad Nasif\n" );
-fwrite( $fp, "Zawad Nazib\n" );
+    // Solution temporary
 
-// Solution temporary
+    $data = file_get_contents( $path );
+    fwrite( $fp, $data );
+    fwrite( $fp, "Abduz Zaher\n" );
 
-$data = file_get_contents( $path );
-fwrite( $fp, $data );
-fwrite( $fp, "Abduz Zaher\n" );
+    // another solution 
 
-// another solution 
-
-// Open file in append 'a' mode
-$fp = fopen( $path, 'a' );
+    // Open file in append 'a' mode
+    $fp = fopen( $path, 'a' );
+}
