6eb068cbb6543fa63346f102f97fd317fe89c107
Faad Ragib <ragibfuad@gmail.com>
2022-07-10 15:23:14 +0600

    added code for preg_split()

--- array/array-string.php
+++ array/array-string.php
@@ -6,10 +6,19 @@ $data_array = explode( ', ', $data );
 
 echo $data_array[0]."\n";
 
-// Here preg_split() will go
 
 // join() -> Convert an array to a string
 $data_string = join( ', ', $data_array );
 
 echo $data_string."\n";
+
+// problem with explode, it's work only for one delimeter
+$data = 'Fuad, Zawad, Nazia, Fahad,Nazia, Ragib';
+$data_array = explode( ', ', $data );
+echo count( $data_array );
+
+// Solution is preg_split()
+$data = 'Fuad, Zawad, Nazia, Fahad,Nazia, Ragib';
+$data_array = preg_split( '/, |,/', $data );
+echo count( $data_array );
 
