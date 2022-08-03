88325d34e0867e87776bdbd6f270ce464672f91e
Faad Ragib <ragibfuad@gmail.com>
2022-08-04 02:31:54 +0600

    learning array

---
+++ array/array-list.php
@@ -0,0 +1,14 @@
+<?php
+
+$rgb = [100, 200, 117];
+
+// $r = $rgb[0];
+// $g = $rgb[1];
+// $b = $rgb[2];
+
+// echo $b;
+
+// it can be easily solved by list() function
+
+list( $r, $g, $b ) = $rgb;
+echo $b;
