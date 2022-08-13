31fe28c80ff3153c7545cdea28618bd0782c9854
Faad Ragib <ragibfuad@gmail.com>
2022-08-13 14:08:05 +0600

    anonimous function

--- anonymous-function/af-01.php
+++ anonymous-function/af-01.php
@@ -5,3 +5,22 @@ $x = function ( $var ) {
 }; // anonymous function will end with a semicolon
 
 $x( "Fuad" );
+
+$message = 'Hi';
+$say = function () use ( &$message ) {
+	$message = 'Hello';
+	echo $message;
+};
+
+$say2 = function () use ( $message ) {
+    $message = "Hello";
+    echo $message;
+};
+
+// call by value
+$say2();
+echo $message;
+
+// call by reference
+$say();
+echo $message;
